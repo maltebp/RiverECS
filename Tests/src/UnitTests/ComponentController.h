a6a269a5a1388e8d531edbed00d72e40c0b5cef0
Malte B.P <maltebp94@gmail.com>
2020-11-12 17:57:31 +0100

    Ensured that new components are initialized to default values

--- Tests/src/UnitTests/ComponentController.h
+++ Tests/src/UnitTests/ComponentController.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <catch.h>
 
 #include "Log.h"
@@ -11,6 +12,11 @@ struct TestComponent : public River::ECS::Component {
 	int i;
 	char c;
 	double d;
+};
+
+struct DefaultComponent : public River::ECS::Component {
+	int i = 50;
+	std::string s = "Default";
 };
 
 // Check if Controller's components matches expected components
@@ -114,4 +120,60 @@ TEST_CASE("Component Controller Tests", "[component_controller]") {
 
 		checkEntities(controller, entities, expectedComponents);
 	}
+}
+
+
+// ================================================================================================================================================================================================
+
+TEST_CASE("Component is reset using default constructor on construction", "[component_controller]") {
+
+	River::ECS::Domain domain;
+	River::ECS::ComponentController<DefaultComponent> controller;
+
+	auto entity = domain.createEntity();
+
+	// Create component
+	auto component = controller.createComponent(entity);
+
+	// Test that initial values (before it's moved to primary list)
+	// are correct
+	REQUIRE(component->i == 50);
+	REQUIRE(component->s == "Default");
+
+	controller.clean();
+
+	// Test that values after being moved to primary list
+	// are correct
+	component = controller.getComponent(entity);
+	REQUIRE(component->i == 50);
+	REQUIRE(component->s == "Default");
+
+	// Change values of component
+	component->i = 100; 
+	component->s = "NotDefault";
+
+	controller.clean();
+
+	// Tests values are changed
+	component = controller.getComponent(entity);
+	REQUIRE(component->i == 100);
+	REQUIRE(component->s == "NotDefault");
+
+	// Delete and create new component ---------------------------
+	controller.deleteComponent(entity);
+	controller.clean();
+	component = controller.createComponent(entity);
+
+	// Test default values before moving to primary list
+	REQUIRE(component->i == 50);
+	REQUIRE(component->s == "Default");
+
+	controller.clean();
+
+	// Test that values after being moved to primary list
+	component = controller.getComponent(entity);
+	REQUIRE(component->i == 50);
+	REQUIRE(component->s == "Default");
+
+
 }
